7d5518d5684141715dd810470d3a85f016b7ee55
aplhaprakash <prakashplayboy2002@gmail.com>
2024-12-27 07:37:33 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import preact from '@preact/preset-vite'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server:{
-    proxy:{
-      '/api':{
-        target:'https://slrtech-chatapp.onrender.com/',
-        secure:false
-      }
-    }
-  },
+  plugins: [preact()],
 })
